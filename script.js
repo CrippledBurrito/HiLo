7cdf96ae8afd2fb23284fb96046b67c697143b19
Bruno <bruno.h.student@lyonscommunityschool.org>
2017-03-28 14:27:43 +0000

    wronk or right

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
+var answer = prompt("Im a number 1-10") 
 
+$(document).ready(function () {
+ if (answer > 6 && answer < 8){
+     $("img").attr("src", "https://media.giphy.com/media/lk9MPPlFZfETm/giphy.gif")
+ } else {
+  $("img").attr("src", "https://pbs.twimg.com/profile_images/834814974287544326/fkefG_VY.jpg")
+ }
 });
